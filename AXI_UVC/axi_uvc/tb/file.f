d4a5add2a27a9b99c07f1c8cd167029f415496e4
MUHAMMAD-HAMMAD2K04 <m.hammad2k04@gmail.com>
2026-07-29 21:24:06 +0500

    AXI Updated File list

---
+++ AXI_UVC/axi_uvc/tb/file.f
@@ -0,0 +1,30 @@
+-64
+-sv
+
+-uvmhome /home/cc/mnt/XCELIUM2309/tools/methodology/UVM/CDNS-1.1d
+
+-timescale 1ns/1ns
+
+-incdir /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/sv
+-incdir /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/tb
+
+/home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/sv/axi_if.sv
+
+/home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/sv/axi_pkg.sv
+
+// /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/tb/axi_test_lib.sv
+
+/home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/tb/hw_top.sv
+/home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/tb/axi_tb_top.sv
+
+/home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/Dut/Bridge_CP.sv
+/home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/Dut/Bridge_DP.sv
+/home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/Dut/Bridge_Complete.sv
+
+
+// -access
+// +rwc
+// -gui
+
++UVM_TESTNAME=axi_single_write_seq_test
++UVM_VERBOSITY=UVM_FULL
