87ce7b6ab400a3db72eb461e0a82cc63578535af
MUHAMMAD-HAMMAD2K04 <m.hammad2k04@gmail.com>
2026-07-29 21:12:28 +0500

    AXI_Monitor Updated

--- AXI_UVC/axi_uvc/sv/axi_monitor.sv
+++ AXI_UVC/axi_uvc/sv/axi_monitor.sv
@@ -2,124 +2,61 @@ class axi_monitor extends uvm_monitor;
 
     `uvm_component_utils(axi_monitor)
 
-    // Virtual Interface
     virtual axi_if vif;
-
-    // Analysis Port
     uvm_analysis_port #(axi_transaction) ap;
 
-    // Constructor
     function new(string name="axi_monitor", uvm_component parent);
-        super.new(name,parent);
+        super.new(name, parent);
     endfunction
-      
-    // Build Phase
+
     function void build_phase(uvm_phase phase);
-
         super.build_phase(phase);
-
-        ap = new("ap",this);
-
-        if(!uvm_config_db#(virtual axi_if)::get(this,"","vif",vif))
-        begin
-
-        `uvm_fatal("NOVIF", "AXI virtual interface not found")
-
-        end
-
+        ap = new("ap", this);
+        if(!uvm_config_db#(virtual axi_if)::get(this, "", "vif", vif))
+            `uvm_fatal("NOVIF", "axi_if not found")
     endfunction
 
-    // Run Phase
     task run_phase(uvm_phase phase);
+        axi_transaction tr;
 
-        forever
-        begin
+        forever begin
+            @(vif.mon_cb);
 
-            @(posedge vif.ACLK);
-
-            if(!vif.ARESETn)
+            if(!vif.PRESET)
                 continue;
 
-            // Check write transaction
-            if(vif.awvalid && vif.awready)
-            begin
+            // Monitor detects request read enable pulse from DUT
+            if(vif.mon_cb.req_fifo_rd_en) begin
+                tr = axi_transaction::type_id::create("tr");
 
-                collect_write();
+                tr.write = vif.mon_cb.req_fifo_rd_data[32];
+                tr.addr  = vif.mon_cb.req_fifo_rd_data[31:0];
 
+                // Handles Write transaction capture
+                if(tr.write) begin
+                    do @(vif.mon_cb);
+                    while (!vif.mon_cb.wr_fifo_rd_en);
+
+                    tr.data = vif.mon_cb.wr_fifo_rd_data[31:0];
+                    tr.strb = vif.mon_cb.wr_fifo_rd_data[35:32];
+                end 
+                // Handles Read transaction capture
+                else begin
+                    do @(vif.mon_cb);
+                    while (!vif.mon_cb.rd_fifo_wr_en);
+
+                    tr.data = vif.mon_cb.rd_fifo_wr_data;
+                end
+
+                ap.write(tr);
+
+                `uvm_info(get_type_name(), $sformatf("Recieved Write Transaction:\n%s", tr.sprint()), UVM_MEDIUM)
+
+                `uvm_info(get_type_name(),
+                    $sformatf("MONITOR CAPTURED: ADDR=%h WRITE=%0d DATA=%h", tr.addr, tr.write, tr.data),
+                    UVM_MEDIUM)
             end
-
-            // Check read transaction
-            if(vif.arvalid && vif.arready)
-            begin
-
-                collect_read();
-
-            end
-
         end
-
-    endtask
-
-    // Capture Write Transaction
-    task collect_write();
-
-        axi_transaction tr;
-        tr = axi_transaction::type_id::create("tr");
-
-        tr.write = 1;
-
-        // Capture Address
-        tr.addr = vif.awaddr;
-
-        // Wait for Write Data
-        @(posedge vif.ACLK);
-
-        wait(vif.wvalid && vif.wready);
-
-        tr.data = vif.wdata;
-
-        tr.strb = vif.wstrb;
-         
-        // Wait for Response
-        @(posedge vif.ACLK);
-
-        wait(vif.bvalid && vif.bready);
-
-        tr.resp = vif.bresp;
-
-        ap.write(tr);
-
-        `uvm_info(
-        get_type_name(),$sformatf("WRITE MONITOR : ADDR=%h DATA=%h RESP=%0d", tr.addr, tr.data, tr.resp), UVM_MEDIUM)
-
-    endtask
-
-    // Capture Read Transaction
-    task collect_read();
-
-        axi_transaction tr;
-
-        tr = axi_transaction::type_id::create("tr");
-
-        tr.write = 0;
-
-        // Capture Address
-        tr.addr = vif.araddr;
-         
-        // Wait for Read Data
-        @(posedge vif.ACLK);
-
-        wait(vif.rvalid && vif.rready);
-
-        tr.data = vif.rdata;
-
-        tr.resp = vif.rresp;
-
-        ap.write(tr);
-
-        `uvm_info(
-        get_type_name(), $sformatf("READ MONITOR : ADDR=%h DATA=%h RESP=%0d", tr.addr, tr.data, tr.resp), UVM_MEDIUM)
-
     endtask
 
 endclass
