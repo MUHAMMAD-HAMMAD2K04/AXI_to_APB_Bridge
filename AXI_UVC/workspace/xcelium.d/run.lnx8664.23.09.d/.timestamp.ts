a95a40b44d63198970e38a7f95f34ccbee6905cc
MUHAMMAD-HAMMAD2K04 <m.hammad2k04@gmail.com>
2026-07-29 21:26:04 +0500

    Workspace and Updated Log Files

---
+++ AXI_UVC/workspace/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
@@ -0,0 +1,7 @@
+1785329904 /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/tb/hw_top.sv
+1785127080 /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/Dut/Bridge_CP.sv
+1785335602 /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/Dut/Bridge_DP.sv
+1785338885 /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/tb/axi_tb_top.sv
+1785339677 /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/sv/axi_pkg.sv
+1785127080 /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/Dut/Bridge_Complete.sv
+1785338974 /home/cc/Downloads/AXI_UVC/AXI_to_APB_Bridge/AXI_UVC/axi_uvc/sv/axi_if.sv
