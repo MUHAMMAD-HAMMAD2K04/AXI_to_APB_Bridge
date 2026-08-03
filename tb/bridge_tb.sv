442251cb7b2c80c387623324e49de356e514f790
MUHAMMAD-HAMMAD2K04 <m.hammad2k04@gmail.com>
2026-08-03 22:56:49 +0500

    Connected Reference Model & Scoreboard in Environment's Connect Phase

--- tb/bridge_tb.sv
+++ tb/bridge_tb.sv
@@ -5,20 +5,38 @@ class bridge_tb extends uvm_env;
     `uvm_component_utils(bridge_tb)
 
     apb_env APB_env;
-    axi_env AXI_env;   
+    axi_env AXI_env;  
+    bridge_ref_model ref_model;
+    bridge_scoreboard      scb; 
 
     function new(string name = "bridge_tb", uvm_component parent);
-        super.new(name, parent);
+        super.new(name,parent);
     endfunction
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         APB_env = apb_env::type_id::create("APB_env", this);
         AXI_env = axi_env::type_id::create("AXI_env", this);
+        ref_model = bridge_ref_model::type_id::create("ref_model", this);
+        scb       = bridge_scoreboard::type_id::create("scb", this);
         `uvm_info(get_type_name(), "Build Phase of Testbench executed!", UVM_HIGH)
     endfunction
 
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+
+        // AXI Monitor -> Reference Model
+        AXI_env.agent.monitor.ap.connect(ref_model.axi_imp);
+
+        // Reference Model -> Scoreboard
+        ref_model.expected_apb_port.connect(scb.expected_fifo.analysis_export);
+
+        // APB Monitor -> Scoreboard
+        APB_env.agent.monitor.analysis_port.connect(scb.actual_fifo.analysis_export);
+    
+    endfunction
+
     function void start_of_simulation_phase(uvm_phase phase);
-        `uvm_info(get_type_name(),"Running Simulation---", UVM_HIGH)
+        `uvm_info(get_type_name(),"Running Simulation ...", UVM_HIGH)
     endfunction
 endclass
