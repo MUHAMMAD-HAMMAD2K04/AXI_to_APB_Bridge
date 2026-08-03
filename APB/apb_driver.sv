331ec7c4a97ec99de4e0e079a605f95f764a5624
MUHAMMAD-HAMMAD2K04 <m.hammad2k04@gmail.com>
2026-08-03 22:51:25 +0500

    apb_driver Updated

--- APB/apb_driver.sv
+++ APB/apb_driver.sv
@@ -1,197 +1,180 @@
-// class apb_driver extends uvm_driver #(axi_to_apb_packet);
-//     `uvm_component_utils(apb_driver)
-
-//     virtual interface apb_if.driver_mp vif;
-
-//     int num_sent;
-//     int packet_delay=10;
-
-//     function new(string name = "apb_driver", uvm_component parent);
-//         super.new(name,parent);
-//     endfunction
-
-//     function void start_of_simulation_phase(uvm_phase phase);
-//         `uvm_info(get_type_name(),"Running Simulation---", UVM_HIGH)
-//     endfunction
-
-//     task run_phase(uvm_phase phase);
-//         fork 
-//             send_to_dut();
-//             reset();
-//         join
-//     endtask
-
-//     function void connect_phase(uvm_phase phase);
-//         if(!uvm_config_db#(virtual interface apb_if.driver_mp)::get(this, "","vif", vif))
-//             `uvm_error("NO-VIF", "Missing virtual interface")
-//     endfunction
-
-//     task send_to_dut();
-//         axi_to_apb_packet pkt;  
-        
-//         repeat(packet_delay);
-        
-//         forever begin
-//             @(negedge vif.clk);
-            
-//             if (vif.PTRANSFER) begin
-                
-//                 `uvm_info(get_type_name(),
-//                     $sformatf("APB transfer started: PWRITE=%b, PADDR=0x%0h",
-//                               vif.PWRITE, vif.PADDR), UVM_MEDIUM)
-                
-//                 @(negedge vif.clk);
-                
-//                 if (!vif.PWRITE) begin
-                    
-//                     `uvm_info(get_type_name(),
-//                         "READ transfer waiting for sequencer item...",
-//                         UVM_MEDIUM)
-
-//                     seq_item_port.get_next_item(pkt);
-
-//                     `uvm_info(get_type_name(),
-//                         $sformatf("READ got item: addr=0x%0h, rdata=0x%0h",
-//                                   pkt.addr, pkt.wdata),
-//                         UVM_MEDIUM)
-
-//                     vif.PRDATA <= pkt.wdata;
-//                     vif.PREADY <= 1'b1;
-                    
-//                     @(negedge vif.clk);
-                    
-//                     vif.PREADY <= 1'b0;
-//                     vif.PRDATA <= '0;
-                    
-//                     seq_item_port.item_done();
-//                 end 
-//                 else begin
-//                     vif.PREADY <= 1'b1;
-                
-//                     @(negedge vif.clk);
-                    
-//                     vif.PREADY <= 1'b0;
-//                 end
-
-//                 num_sent++;
-//             end
-//         end
-//     endtask
-
-//     task reset();
-//         @(negedge vif.clk);
-//         vif.PRDATA<=0;
-//         vif.PREADY<=0;
-//     endtask
-// endclass
-
 class apb_driver extends uvm_driver #(axi_to_apb_packet);
 
-  `uvm_component_utils(apb_driver)
+    `uvm_component_utils(apb_driver)
 
-  virtual apb_if.driver_mp vif;
+    virtual apb_if.driver_mp vif;
 
-  // Simple APB slave memory
-  bit [31:0] mem [bit[31:0]];
+    // APB Slave Memory Model
+    bit [31:0] mem [bit [31:0]];
 
-  int num_trans;
+    // Statistics
+    int num_reads;
+    int num_writes;
 
-  function new(string name="apb_driver",
-               uvm_component parent);
-    super.new(name,parent);
-  endfunction
+    // Wait State Configuration
+    int cfg_wait_cycles;
+    bit random_wait = 1;
 
-  function void connect_phase(uvm_phase phase);
+    rand int unsigned rand_wait_cycles;
 
-    if(!uvm_config_db#(virtual apb_if.driver_mp)::get(
-            this,"","vif",vif))
-      `uvm_fatal("NOVIF","APB interface not found");
+    constraint c_wait {
+        rand_wait_cycles inside {[0:5]};
+    }
 
-  endfunction
+    // Constructor
+    function new(string name="apb_driver",
+                 uvm_component parent);
+        super.new(name,parent);
+    endfunction
 
+    // Build Phase
+    function void build_phase(uvm_phase phase);
 
-  task run_phase(uvm_phase phase);
+        super.build_phase(phase);
 
-    reset_signals();
+        if(!uvm_config_db#(virtual apb_if.driver_mp)::get(
+                this,"","vif",vif))
+            `uvm_fatal("NOVIF","APB Interface not found")
 
-    wait(vif.rst);
-
-    forever begin
-
-       
-      // Wait for APB SETUP
-       
-      @(posedge vif.clk);
-
-      if(!vif.PTRANSFER)
-        continue;
-
-      `uvm_info(get_type_name(),
-      $sformatf("APB Transfer Started WRITE=%0d ADDR=%08h",
-                 vif.PWRITE,
-                 vif.PADDR),
-                 UVM_MEDIUM)
-
-       
-      // ACCESS PHASE
-       
-      @(posedge vif.clk);
-
-       
-      // WRITE
-       
-      if(vif.PWRITE) begin
-
-        mem[vif.PADDR] = vif.PWDATA;
-
-        `uvm_info(get_type_name(),
-        $sformatf("WRITE : MEM[%08h]=%08h",
-                   vif.PADDR,
-                   vif.PWDATA),
-                   UVM_MEDIUM)
-
-      end
-
-       
-      // READ
-       
-      else begin
-
-        if(mem.exists(vif.PADDR))
-          vif.PRDATA <= mem[vif.PADDR];
+         
+        // If test provides wait_cycles use that.
+        // Otherwise driver randomizes them.
+         
+        if(uvm_config_db#(int)::get(
+                this,"","wait_cycles",cfg_wait_cycles))
+            random_wait = 0;
         else
-          vif.PRDATA <= '0;
+            random_wait = 1;
+
+    endfunction
+     
+    // Run Phase
+    task run_phase(uvm_phase phase);
+
+        reset_signals();
+
+        wait(vif.rst);
+
+        forever begin
+
+             
+            // Wait for APB SETUP Phase
+            @(posedge vif.clk);
+
+            if(!vif.PTRANSFER)
+                continue;
+
+            `uvm_info(get_type_name(),
+            $sformatf(
+            "\n----------------------------------\
+             \n APB TRANSFER START\
+             \n TYPE  : %s\
+             \n ADDR  : %08h\
+             \n WDATA : %08h\
+             \n----------------------------------",
+             vif.PWRITE ? "WRITE":"READ",
+             vif.PADDR,
+             vif.PWDATA),
+             UVM_MEDIUM)
+
+             
+            // ACCESS PHASE
+            @(posedge vif.clk);
+
+            // Determine Wait States
+            vif.PREADY <= 0;
+
+            if(random_wait)
+                assert(randomize(rand_wait_cycles));
+            else
+                rand_wait_cycles = cfg_wait_cycles;
+
+            repeat(rand_wait_cycles)
+                @(posedge vif.clk);
+
+             
+            // WRITE Operatio
+            if(vif.PWRITE) begin
+
+                mem[vif.PADDR] = vif.PWDATA;
+
+                num_writes++;
+
+                `uvm_info(get_type_name(),
+                $sformatf("WRITE : MEM[%08h] = %08h",
+                          vif.PADDR,
+                          vif.PWDATA),
+                          UVM_HIGH)
+
+            end
+
+             
+            // READ Operation
+
+            else begin
+
+                if(mem.exists(vif.PADDR))
+                    vif.PRDATA <= mem[vif.PADDR];
+                else
+                    vif.PRDATA <= 32'h00000000;
+
+                num_reads++;
+
+                `uvm_info(get_type_name(),
+                $sformatf("READ : MEM[%08h] -> %08h",
+                          vif.PADDR,
+                          mem.exists(vif.PADDR) ?
+                          mem[vif.PADDR] : 32'h0),
+                          UVM_HIGH)
+
+            end
+
+             
+            // Complete Transfer
+            vif.PREADY <= 1'b1;
+
+            @(posedge vif.clk);
+
+             
+            // Return to Idle
+            vif.PREADY <= 1'b0;
+            vif.PRDATA <= '0;
+
+            `uvm_info(get_type_name(),
+                      "APB Transfer Completed",
+                      UVM_MEDIUM);
+
+        end
+
+    endtask
+     
+    // Reset Task
+    task reset_signals();
+
+        vif.PREADY <= 0;
+        vif.PRDATA <= 0;
+
+    endtask
+
+
+     
+    // Report Phase
+    function void report_phase(uvm_phase phase);
+
+        super.report_phase(phase);
 
         `uvm_info(get_type_name(),
-        $sformatf("READ : MEM[%08h]=%08h",
-                   vif.PADDR,
-                   vif.PRDATA),
-                   UVM_MEDIUM)
+        $sformatf(
+        "\n====================================\
+         \n APB DRIVER REPORT\
+         \n------------------------------------\
+         \n WRITE Transactions : %0d\
+         \n READ Transactions  : %0d\
+         \n====================================",
+         num_writes,
+         num_reads),
+         UVM_LOW);
 
-      end
-
-       
-      // Tell DUT transaction completed
-       
-      vif.PREADY <= 1'b1;
-
-      @(posedge vif.clk);
-
-      vif.PREADY <= 1'b0;
-      vif.PRDATA <= '0;
-
-      num_trans++;
-
-    end
-
-  endtask
-
-
-  task reset_signals();
-
-    vif.PREADY <= 0;
-    vif.PRDATA <= 0;
-
-  endtask
+    endfunction
 
 endclass
