13255af991ca8ac311aa76fdf1c1aa513f26b3b2
MUHAMMAD-HAMMAD2K04 <m.hammad2k04@gmail.com>
2026-08-03 22:57:38 +0500

    Added some more tests

--- tb/bridge_test_lib.sv
+++ tb/bridge_test_lib.sv
@@ -12,6 +12,7 @@ class axi_base_test extends uvm_test;
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
+        uvm_config_db#(int)::set(this,"*agent.driver*","wait_cycles",0);
         tb = bridge_tb::type_id::create("tb", this);
     endfunction
 
@@ -27,36 +28,6 @@ class axi_base_test extends uvm_test;
 endclass
 
 
-// Five Write Sequence Test
-class axi_five_write_seq_test extends axi_base_test;
-
-    `uvm_component_utils(axi_five_write_seq_test)
-
-
-    function new(string name="axi_five_write_seq_test",
-                 uvm_component parent);
-        super.new(name,parent);
-    endfunction
-
-
-    function void build_phase(uvm_phase phase);
-
-        super.build_phase(phase);
-
-
-        uvm_config_db#(uvm_object_wrapper)::set(
-            this,
-            "tb.AXI_env.agent.sequencer.run_phase",
-            "default_sequence",
-            axi_five_write_seq::type_id::get()
-        );
-
-    endfunction
-
-
-endclass
-
- 
 // Reset Sequence Test
 class reset_seq_test extends axi_base_test;
 
@@ -141,5 +112,107 @@ class axi_read_seq_test extends axi_base_test;
         );
 
     endfunction
+
+endclass
+
+
+// Five Write Sequence Test
+class axi_five_write_seq_test extends axi_base_test;
+
+    `uvm_component_utils(axi_five_write_seq_test)
+
+
+    function new(string name="axi_five_write_seq_test",
+                 uvm_component parent);
+        super.new(name,parent);
+    endfunction
+
+
+    function void build_phase(uvm_phase phase);
+
+        super.build_phase(phase);
+
+
+        uvm_config_db#(uvm_object_wrapper)::set(
+            this,
+            "tb.AXI_env.agent.sequencer.run_phase",
+            "default_sequence",
+            axi_five_write_seq::type_id::get()
+        );
+
+    endfunction
+
+
+endclass
+
+// Wait State Sequence Test
+class wait_state_test extends axi_base_test;
+
+    `uvm_component_utils(wait_state_test)
+
+
+    function new(string name="wait_state_test",
+                 uvm_component parent);
+        super.new(name,parent);
+    endfunction
+
+function void build_phase(uvm_phase phase);
+
+    super.build_phase(phase);
+
+    uvm_config_db#(int)::set(
+        this,
+        "*agent.driver*",
+        "wait_cycles",
+        3
+    );
+
+    uvm_config_db#(uvm_object_wrapper)::set(
+        this,
+        "tb.APB_env.agent.sequencer.run_phase",
+        "default_sequence",
+        axi_wait_state_seq::type_id::get()
+        );
+
+endfunction
+
+endclass
+
+
+// Stress Sequence Test
+class stress_test extends axi_base_test;
+
+    `uvm_component_utils(stress_test)
+
+
+    function new(string name="stress_test",
+                 uvm_component parent);
+        super.new(name,parent);
+    endfunction
+
+function void build_phase(uvm_phase phase);
+
+    int delay;
+
+    super.build_phase(phase);
+
+    delay = $urandom_range(0,5);
+
+    uvm_config_db#(int)::set(
+        this,
+        "*agent.driver*",
+        "wait_cycles",
+        delay
+    );
+
+    uvm_config_db#(uvm_object_wrapper)::set(
+        this,
+        "tb.APB_env.agent.sequencer.run_phase",
+        "default_sequence",
+        axi_stress_seq::type_id::get()
+    );
+
+
+endfunction
 
 endclass
